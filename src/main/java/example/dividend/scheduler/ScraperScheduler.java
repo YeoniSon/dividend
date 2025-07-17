eb66879f48aa679b373ef05300dae9762e561049
YeoniSon <sjy8211@gmail.com>
2025-07-18 00:17:08 +0900

    log를 남기는 코드

--- src/main/java/example/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/example/dividend/scheduler/ScraperScheduler.java
@@ -67,6 +67,7 @@ public class ScraperScheduler {
 
                         if (!exists) {
                             this.dividendRepository.save(e);
+                            log.info("insert new dividend -> " + e.toString());
                         }
                     });
 
